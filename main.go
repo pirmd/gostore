0459954f233696bccf296a91bade660e185ff718
Πrmd <pirmd@users.noreply.github.com>
2020-06-23 10:57:36 +0200

    Add configuration options for the indexing analyzer

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	_ "github.com/pirmd/gostore/modules/dehtmlizer"
 	_ "github.com/pirmd/gostore/modules/googlebooks"
 	_ "github.com/pirmd/gostore/modules/organizer"
+
+	_ "github.com/blevesearch/bleve/analysis/lang/en"
+	_ "github.com/blevesearch/bleve/analysis/lang/fr"
 )
 
 func main() {
